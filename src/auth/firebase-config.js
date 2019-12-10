3ca783a23dead7cc4327e768cfce1b8396e80dbd
Zac Braddy <zacharybraddy@gmail.com>
2019-12-10 22:10:17 +0000

    Merging in trying to get the CI to work! (#8)
    * Basic create react app solution
    
    * Feature/initial (#1)
    
    * Updated the logo
    
    * Created navbar
    
    * Created the admin waiting list table
    
    * Completed dnd functionality
    
    * Added a fab to edit the admin table and a draw at the bottom of the
    table to edit and save
    
    * Refactored out components separately and made the state be controlled by
    a reducer and made it so that you can add a row
    
    * Added configuration vars in preparation to bring in firebase stuff
    
    * Added circleci
    
    * Add firebase and update to circle ci workflows
    
    * Trying to update filters to match feature branches
    
    * Added deployment to develop
    
    * Feature/add user screen (#4)
    
    * Added user waiting list
    
    * Broken commit: Attempt made at firebase auth still some problems with it
    though
    
    * Completed implementing authentication
    
    * Test was causing failed builds because firebase auth was setting it off
    and it wasn't helping anyway
    
    * Added .env creation scripts to circle ci build
    
    * You aren't the boss of me!!
    
    * Updating develop env building
    
    * Hopefully it builds this time
    
    * Fixed whoopsie in dotenv setup
    
    * Removed useless config file, change env vars to just rely on ci to input (#7)
    
    the correct values rather than expecting two different versions of them

--- src/auth/firebase-config.js
+++ src/auth/firebase-config.js
@@ -6,30 +6,14 @@ import 'firebase/database';
 // import { createFirestoreInstance, firestoreReducer } from 'redux-firestore' // <- needed if using firestore
 
 const fbConfig = {
-  apiKey:
-    process.env.REACT_APP_FIREBASE_PROD_API_KEY ||
-    process.env.REACT_APP_FIREBASE_DEV_API_KEY,
-  authDomain:
-    process.env.REACT_APP_FIREBASE_PROD_AUTH_DOMAIN ||
-    process.env.REACT_APP_FIREBASE_DEV_AUTH_DOMAIN,
-  databaseURL:
-    process.env.REACT_APP_FIREBASE_PROD_DATABASE_URL ||
-    process.env.REACT_APP_FIREBASE_DEV_DATABASE_URL,
-  projectId:
-    process.env.REACT_APP_FIREBASE_PROD_PROJECT_ID ||
-    process.env.REACT_APP_FIREBASE_DEV_PROJECT_ID,
-  storageBucket:
-    process.env.REACT_APP_FIREBASE_PROD_STORAGE_BUCKET ||
-    process.env.REACT_APP_FIREBASE_DEV_STORAGE_BUCKET,
-  messagingSenderId:
-    process.env.REACT_APP_FIREBASE_PROD_MESSAGING_SENDER_ID ||
-    process.env.REACT_APP_FIREBASE_DEV_MESSAGING_SENDER_ID,
-  appId:
-    process.env.REACT_APP_FIREBASE_PROD_APP_ID ||
-    process.env.REACT_APP_FIREBASE_DEV_APP_ID,
-  measurementId:
-    process.env.REACT_APP_FIREBASE_PROD_MEASUREMENT_ID ||
-    process.env.REACT_APP_FIREBASE_DEV_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const rrfConfig = {
